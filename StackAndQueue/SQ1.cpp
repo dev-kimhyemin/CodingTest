690101d6566fb3bb0518d64bc6e5d7512a0de848
dev.kimhyemin <dev.kimhyemin@gmail.com>
2024-09-10 20:08:22 +0900

    Add StackAndQueue examples

---
+++ StackAndQueue/SQ1.cpp
@@ -0,0 +1,45 @@
+﻿#include <vector>
+#include <iostream>
+
+using namespace std;
+
+vector<int> solution(vector<int> arr) 
+{
+    vector<int> answer;
+
+    for(const int number : arr)
+    {
+        if(answer.empty() || answer.back() != number)
+        {
+            answer.emplace_back(number);
+        }
+    }
+
+    return answer;
+}
+
+// int main()
+// {
+//     const vector<int> arr({1,1,3,3,0,1,1});
+//     const vector<int> answer = solution(arr);
+//
+//     for(const int num : answer)
+//     {
+//         cout << num << " ";
+//     }
+// }
+
+
+
+/*문제 설명
+배열 arr가 주어집니다. 배열 arr의 각 원소는 숫자 0부터 9까지로 이루어져 있습니다.
+이때, 배열 arr에서 연속적으로 나타나는 숫자는 하나만 남기고 전부 제거하려고 합니다.
+단, 제거된 후 남은 수들을 반환할 때는 배열 arr의 원소들의 순서를 유지해야 합니다. 예를 들면,
+
+arr = [1, 1, 3, 3, 0, 1, 1] 이면 [1, 3, 0, 1] 을 return 합니다.
+arr = [4, 4, 4, 3, 3] 이면 [4, 3] 을 return 합니다.
+배열 arr에서 연속적으로 나타나는 숫자는 제거하고 남은 수들을 return 하는 solution 함수를 완성해 주세요.
+
+제한사항
+배열 arr의 크기 : 1,000,000 이하의 자연수
+배열 arr의 원소의 크기 : 0보다 크거나 같고 9보다 작거나 같은 정수*/
